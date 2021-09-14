fd0663c1811656e330567e30677d8ac9281c4292
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-09-14 11:58:09 +0100

    Add prefix for profile

--- profiles.nix
+++ profiles.nix
@@ -1,12 +1,14 @@
 dotfiles-manager: modules: collections: {
-  macbook2017 = (dotfiles-manager.lib.createProfile (collections.user.devMachine
-    ++ collections.system.guiSystemMinimal ++ [
-      modules.windowManager.leftwm
-      modules.multiplexer.tmux
-      modules.terminal.wezterm
-      modules.shell.nushell
-      modules.bin.neovim
-      modules.misc.font
-      modules.misc.bibata-cursor
-    ]));
+  nixos = {
+    macbook2017 = (dotfiles-manager.lib.createProfile
+      (collections.user.devMachine ++ collections.system.guiSystemMinimal ++ [
+        modules.windowManager.leftwm
+        modules.multiplexer.tmux
+        modules.terminal.wezterm
+        modules.shell.nushell
+        modules.bin.neovim
+        modules.misc.font
+        modules.misc.bibata-cursor
+      ]));
+  };
 }
