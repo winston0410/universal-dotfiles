12e5a822149651b4f605ae27f8d74a0d7bc4b076
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-09-14 13:07:22 +0100

    Update cursor config based on upstream

--- modules/misc/bibata-cursor.nix
+++ modules/misc/bibata-cursor.nix
@@ -2,18 +2,10 @@ username:
 { pkgs, ... }:
 
 {
-  home-manager.users.${username} = {
-    home.packages = with pkgs; [ bibata-cursors ];
-
-    home.sessionVariables = {
-      XCURSOR_SIZE = "80";
-      XCURSOR_THEME = "Bibata_Ice";
-    };
-    home.file = {
-      ".icons/default" = {
-        source = "${pkgs.bibata-cursors}/share/icons/Bibata_Ice";
-      };
-    };
+  dotfiles.xserver.cursor = {
+    enable = true;
+    package = pkgs.bibata-cursors;
+    theme = "Bibata_Ice";
+    size = 80;
   };
-
 }
