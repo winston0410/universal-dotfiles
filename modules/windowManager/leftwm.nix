36479952bd1dbab7809045e3a887bbca2168b066
kghugo <hugosum.dev@protonmail.com>
2021-09-15 11:00:43 +0100

    Inject leftwm theme

--- modules/windowManager/leftwm.nix
+++ modules/windowManager/leftwm.nix
@@ -1,7 +1,14 @@
-_: { pkgs, config, ... }: {
+username:
+{ pkgs, config, ... }: {
   dotfiles.terminal.windowManager = {
     enable = true;
     package = pkgs.leftwm;
     configPath = ../../dotfiles/leftwm/config.toml;
+  };
+
+  home-manager.users.${username} = {
+    xdg.configFile = {
+      "leftwm/themes" = { source = ../../dotfiles/leftwm/themes; };
+    };
   };
 }
