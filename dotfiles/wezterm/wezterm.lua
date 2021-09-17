2de07d799d26caf83f3cf08442a66c31730768e0
kghugo <hugosum.dev@protonmail.com>
2021-09-17 07:48:50 +0100

    Remove padding

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -9,10 +9,10 @@ return {
 	font_size = 30.0,
 	enable_tab_bar = false,
 	window_padding = {
-		left = 5,
-		right = 5,
-		top = 5,
-		bottom = 5,
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
 	},
 	color_scheme = "tokyonight_night",
 	color_schemes = {
