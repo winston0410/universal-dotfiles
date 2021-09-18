c20c0a14cd4dfa688310e337b492920976e7de9c
kghugo <hugosum.dev@protonmail.com>
2021-09-18 21:00:46 +0100

    Add gtk-key-theme

--- modules/gui/brave.nix
+++ modules/gui/brave.nix
@@ -6,5 +6,10 @@ username:
     home.sessionVariables = { BROWSER = "brave"; };
 
     home.packages = with pkgs; [ brave ];
+
+    xdg.configFile = { "gtk-3.0/setting.ini" = { text = ''
+    [Settings]
+    gtk-key-theme-name = Emacs
+    ''; }; };
   };
 }
