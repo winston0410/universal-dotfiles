115a229d1dfc499c5444e89b51f1e61d2015ee80
kghugo <hugosum.dev@protonmail.com>
2021-09-20 12:13:51 +0100

    Try install eww directly

--- modules/bar/eww.nix
+++ modules/bar/eww.nix
@@ -1,16 +1,9 @@
 username:
 { pkgs, config, ... }: {
-  dotfiles.xserver.bar = {
-    enable = false;
-    package = pkgs.eww;
-  };
+  # dotfiles.xserver.bar = {
+  # enable = false;
+  # package = pkgs.eww;
+  # };
 
-  # home-manager.users.${username} = {
-  # home.packages = [ pkgs.feh ];
-  # xdg.configFile = {
-  # "leftwm/themes/current/" = {
-  # source = ../../dotfiles/leftwm/themes/current;
-  # };
-  # };
-  # };
+  home-manager.users.${username} = { home.packages = [ pkgs.eww ]; };
 }
