f222ce602443795e0c5f0860bd53b2fe1f67853e
kghugo <hugosum.dev@protonmail.com>
2021-09-20 22:15:26 +0100

    Change profile

--- profiles.nix
+++ profiles.nix
@@ -1,25 +1,25 @@
 { inputs, modules, collections }: {
-  nixos = {
-    macbook2017 = (inputs.dotfiles-manager.lib.mkSystem {
-      system = "x86_64-linux";
-      username = "hugosum";
-      modules = [
-        (inputs.dotfiles-manager.lib.mkProfile (collections.user.devMachine
-          ++ collections.system.guiSystemMinimal ++ [
-            modules.windowManager.leftwm
-            modules.service.nixos.hydra
-            modules.bin.maim
-            modules.multiplexer.tmux
-            modules.terminal.wezterm
-            modules.shell.zsh
-            modules.bin.neovim
-            modules.bin.nix-direnv
-            modules.gui.telegram
-            modules.misc.font
-            modules.misc.bibata-cursor
-          ]) username)
-      ];
-      inherit inputs;
-    });
+  user = {
+    dev = (inputs.dotfiles-manager.lib.mkProfile (collections.user.devMachine
+      ++ [
+        modules.windowManager.leftwm
+        modules.service.nixos.hydra
+        modules.bin.maim
+        modules.multiplexer.tmux
+        modules.terminal.wezterm
+        modules.shell.zsh
+        modules.bin.neovim
+        modules.bin.nix-direnv
+        modules.gui.telegram
+        modules.misc.font
+        modules.misc.bibata-cursor
+      ]));
   };
+
+  # make the system here, so that the inputs of universal dotfiles will be used
+  nixos = (inputs.dotfiles-manager.lib.mkSystem {
+    inherit inputs;
+    modules = [ collections.system.guiSystemMinimal ];
+    system = "x86_64-linux";
+  });
 }
