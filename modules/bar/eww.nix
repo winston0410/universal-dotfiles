69a7c37586b1338c9cdca425eea9078b0b38a2bc
kghugo <hugosum.dev@protonmail.com>
2021-09-21 12:30:02 +0100

    Create overlay

--- modules/bar/eww.nix
+++ modules/bar/eww.nix
@@ -1,7 +1,16 @@
 username:
-{ pkgs, config, ... }: {
-  dotfiles.xserver.bar = {
-    enable = false;
-    package = pkgs.eww;
+{ inputs, pkgs, config, ... }:
+let
+  # TODO: Should the overlay be made here?
+  overlay = final: prev: {
+    "eww" = inputs.eww.packages.${builtins.currentSystem}.eww;
   };
+in {
+  nixpkgs.overlays = [ overlay ];
+  environment.systemPackages = [ pkgs.eww ];
+  
+  # dotfiles.xserver.bar = {
+    # enable = false;
+    # package = pkgs.eww;
+  # };
 }
