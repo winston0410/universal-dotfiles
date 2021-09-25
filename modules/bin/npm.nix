45e99c9ba90467afca3709d55a6d2d4e3de2f80d
kghugo <hugosum.dev@protonmail.com>
2021-09-25 07:29:10 +0100

    Add yarnrc config

--- modules/bin/npm.nix
+++ modules/bin/npm.nix
@@ -7,6 +7,15 @@ in {
       text = (builtins.readFile ../../dotfiles/.npmrc) + ''
         # Cache directory for NPM
         cache=${xdg.cacheHome}/npm
+        update-notifier=false
+      '';
+    };
+
+    # Not going to use yarn anymore, but keeping this just in case
+    ".yarnrc" = {
+      text = ''
+        yarn-offline-mirror "${xdg.cacheHome}/.yarn-cache"
+        disable-self-update-check true
       '';
     };
   };
