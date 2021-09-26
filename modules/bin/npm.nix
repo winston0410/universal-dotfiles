b4f8ce3297fa11bf43cbbb4f067655c08d3e0785
kghugo <hugosum.dev@protonmail.com>
2021-09-26 20:22:10 +0100

    Move yarn and .npmrc into config home

--- modules/bin/npm.nix
+++ modules/bin/npm.nix
@@ -2,9 +2,10 @@
 
 let xdg = config.xdg;
 in {
-  home.file = {
-    ".npmrc" = {
-      text = (builtins.readFile ../../dotfiles/.npmrc) + ''
+  xdg.configFile = {
+    "npm/npmrc" = {
+      text = (builtins.readFile ../../dotfiles/npm/npmrc) + ''
+        prefix="${xdg.dataHome}/npm
         # Cache directory for NPM
         cache=${xdg.cacheHome}/npm
         # Cache directory for PNPM
@@ -14,13 +15,20 @@ in {
     };
 
     # Not going to use yarn anymore, but keeping this just in case
-    ".yarnrc" = {
+    "yarn/yarnrc" = {
       text = ''
-        yarn-offline-mirror "${xdg.cacheHome}/.yarn-cache"
+        yarn-offline-mirror "${xdg.cacheHome}/yarn"
         disable-self-update-check true
       '';
     };
   };
 
-  home.sessionVariables = { NODE_REPL_HISTORY = "${xdg.cacheHome}/node_repl_history"; };
+  home.sessionVariables = {
+    NPM_CONFIG_USERCONFIG = "${xdg.configHome}/npm/npmrc";
+    NODE_REPL_HISTORY = "${xdg.cacheHome}/node_repl_history";
+  };
+
+  dotfiles-manager.shellAliases = {
+    yarn = ''yarn --use-yarnrc "${xdg.configHome}/yarn/yarnrc"'';
+  };
 }
