01a53b9320406c9b1bc722153989e87c47f7f185
kghugo <hugosum.dev@protonmail.com>
2021-09-26 19:00:01 +0100

    Add notice for fixing docker configuration

--- modules/service/docker.nix
+++ modules/service/docker.nix
@@ -1,6 +1,8 @@
 username: { config, lib, pkgs, ... }:
 
 {
+  # set DOCKER_CONFIG=$XDG_CONFIG_HOME/docker
+
   environment.variables = { DOCKER_BUILDKIT = "1"; };
 
   environment.systemPackages = with pkgs; [
