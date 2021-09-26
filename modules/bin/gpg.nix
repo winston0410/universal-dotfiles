67c9ceace78fdde89ba2a33aa82b564831cd81c6
kghugo <hugosum.dev@protonmail.com>
2021-09-26 19:38:38 +0100

    Fix gnupg home directory path

--- modules/bin/gpg.nix
+++ modules/bin/gpg.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }:
 
-{
+let xdg = config.xdg;
+in {
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/gpg.nix
   programs.gpg = {
     enable = true;
     # convert file into set from string?
     settings = { };
+    homedir = "${xdg.dataHome}/gnupg";
   };
 }
