b8a04c051b1e08ddcc44e93f553454a1a2a42b8e
kghugo <hugosum.dev@protonmail.com>
2021-09-27 09:39:03 +0100

    Fix home-manager lib path

--- modules/service/mysql.nix
+++ modules/service/mysql.nix
@@ -1,13 +1,22 @@
 username:
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let xdg = config.home-manager.users.${username}.xdg;
 in {
-  services.mysql = { enable = true; };
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql80;
+  };
 
   home-manager.users.${username} = {
+    home.activation = {
+      mysql-cache-prep =
+        inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          $DRY_RUN_CMD mkdir $VERBOSE_ARG -p '${xdg.cacheHome}/mysql'
+        '';
+    };
     home.sessionVariables = {
-      MYSQL_HISTFILE = "${xdg.cacheHome}/mysql_history";
+      MYSQL_HISTFILE = "${xdg.cacheHome}/mysql/history";
     };
   };
 }
--- modules/service/postgres.nix
+++ modules/service/postgres.nix
@@ -1,24 +1,23 @@
 username:
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 let xdg = config.home-manager.users.${username}.xdg;
 in {
   services.postgresql = { enable = true; };
 
-  home.activation = {
-    pg-cache-prep = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      $DRY_RUN_CMD mkdir $VERBOSE_ARG -p '${xdg.cacheHome}/pg'
-    '';
-  };
-
   home-manager.users.${username} = {
+    home.activation = {
+      pg-cache-prep = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD mkdir $VERBOSE_ARG -p '${xdg.cacheHome}/pg'
+      '';
+    };
     home.sessionVariables = {
       PSQLRC = "${xdg.configHome}/pg/psqlrc";
       PSQL_HISTORY = "${xdg.cacheHome}/pg/psql_history";
       PGPASSFILE = "${xdg.cacheHome}/pg/pgpass";
       PGSERVICEFILE = "${xdg.cacheHome}/pg/pg_service.conf";
     };
-  };
 
-  xdg.configFile = { "pg/" = { source = ../../dotfiles/pg; }; };
+    xdg.configFile = { "pg/" = { source = ../../dotfiles/pg; }; };
+  };
 }
