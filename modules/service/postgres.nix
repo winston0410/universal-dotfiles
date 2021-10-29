6fd1b54ba9b7a571d3c7a1427d8a1fb776482292
kghugo <hugosum.dev@protonmail.com>
2021-10-29 10:57:44 +0100

    Disable all password for local psql

--- modules/service/postgres.nix
+++ modules/service/postgres.nix
@@ -7,6 +7,8 @@ in {
     enable = true; 
     authentication = ''
         local all postgres trust
+        host  all all 127.0.0.1/32 trust
+        host  all all ::1/128      trust
     '';
     package = pkgs.postgresql_14;
   };
