633f464e597c88f90436d29d766518d06e342e50
kghugo <hugosum.dev@protonmail.com>
2021-10-29 09:06:59 +0100

    Set permission for psql

--- modules/service/postgres.nix
+++ modules/service/postgres.nix
@@ -3,14 +3,10 @@ username:
 
 let xdg = config.home-manager.users.${username}.xdg;
 in {
-  services.postgresql = { enable = true; };
-
-  # home-manager.users.${username} = {
-    # home.activation = {
-      # pg-cache-prep = inputs.home-manager.lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        # $DRY_RUN_CMD mkdir $VERBOSE_ARG -p '${xdg.cacheHome}/pg'
-      # '';
-    # };
-    # xdg.configFile = { "pg/" = { source = ../../dotfiles/pg; }; };
-  # };
+  services.postgresql = { 
+    enable = true; 
+    authentication = lib.mkForce ''
+        local all postgres trust
+    '';
+  };
 }
